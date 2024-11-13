e2d1def7a78f187daf77e4e729483d1db8a4cf2e
willfrw <william.wang_dev@outlook.com>
2024-11-13 20:08:24 -0300

    Update mergePDF.py

--- mergePDF.py
+++ mergePDF.py
@@ -1,10 +1,36 @@
 import PyPDF2
 import os
+from pathlib import Path
 
-def listar_arquivos(diretorio):
+def listar_arquivos(diretorio, lista_arquivos):
     arquivos = os.listdir(diretorio)
     for arquivo in arquivos:
-        print(arquivo)
+        if '.pdf' in arquivo:
+            lista_arquivos.append(arquivo)
+
+def merge_pdf(diretorio, lista_arquivo_pdf, nome_arquivo_mergeado):
+    merge = PyPDF2.PdfFileMerger()
+    
+    for arquivo in lista_arquivo_pdf:
+        merge.append(arquivo)
+
+    merge.write(nome_arquivo_mergeado)
+
+diretorio = "c://Users//willi//OneDrive//Documentos//Projetos Computação//mergepdf"
+
+lista_pdfs = []
+
+listar_arquivos(diretorio, lista_pdfs)
+
+print(lista_pdfs)
+
+lista_arq_lido = []
+
+for pdf in lista_pdfs:
+    arquivo = open(pdf, 'rb')
+    lista_arq_lido.append(PyPDF2.PdfFileReader(arquivo))
+
+merge_pdf(lista_arq_lido, "teste_merge.pdf")
 
 
 '''
