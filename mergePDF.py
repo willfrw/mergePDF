71718c5e5efb4ba67886cc59db1691ae0dee5565
willfrw <william.wang_dev@outlook.com>
2024-11-14 15:55:13 -0300

    pdf file merger

--- mergePDF.py
+++ mergePDF.py
@@ -8,14 +8,15 @@ def listar_arquivos(diretorio, lista_arquivos):
         if '.pdf' in arquivo:
             lista_arquivos.append(arquivo)
 
-def merge_pdf(diretorio, lista_arquivo_pdf, nome_arquivo_mergeado):
-    merge = PyPDF2.PdfFileMerger()
+def merge_pdf(lista_arquivo_pdf, nome_arquivo_mergeado):
+    merge = PyPDF2.PdfMerger()
     
     for arquivo in lista_arquivo_pdf:
         merge.append(arquivo)
 
     merge.write(nome_arquivo_mergeado)
 
+# colocar diretório de onde estão os arquivos pdf - depois devo incorporar algo que já pega o diretório automaticamente
 diretorio = "c://Users//willi//OneDrive//Documentos//Projetos Computação//mergepdf"
 
 lista_pdfs = []
@@ -28,7 +29,7 @@ lista_arq_lido = []
 
 for pdf in lista_pdfs:
     arquivo = open(pdf, 'rb')
-    lista_arq_lido.append(PyPDF2.PdfFileReader(arquivo))
+    lista_arq_lido.append(PyPDF2.PdfReader(arquivo))
 
 merge_pdf(lista_arq_lido, "teste_merge.pdf")
 
